3baa65fd181b98366a6df18f34301c5004adbf73
TongYangYang <780957054@qq.com>
2019-10-25 20:47:10 +0800

    简单修改

--- git-test-project/src/com/tyy/git/FirstClass.java
+++ git-test-project/src/com/tyy/git/FirstClass.java
@@ -5,13 +5,13 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("hello git project");
-
+        // hello test
     }
 
 
     public void hello(){
         int i = 0;
         i ++;
-        System.out.println(i);
+        System.out.println(++i);
     }
 }
