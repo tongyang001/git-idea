836d5b13bb1c5719f3fe476dc179652e382a83ae
TongYangYang <780957054@qq.com>
2019-10-25 19:54:55 +0800

    删除没用的代码

--- git-test-project/src/com/tyy/git/FirstClass.java
+++ git-test-project/src/com/tyy/git/FirstClass.java
@@ -3,13 +3,8 @@ package com.tyy.git;
 public class FirstClass {
 
     public static void main(String[] args) {
+
         System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
+
     }
 }
